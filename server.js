3180ac262afb099a64d3341a098f276a623f5e48
Adis <archive.hadisty@gmail.com>
2024-09-03 09:51:49 +0700

    config project fixing

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const bodyparser = require('body-parser');
+const express = require('express')
+const bodyparser = require('body-parser')
 // const cors = require('cors');
-const apps = express();
-const PORT = process.env.PORT || 8080;
+const apps = express()
+const PORT = process.env.PORT || 8080
 // const corsoption = { origin: 'http://localhost:3000' }
 
 // router
 // const book = require('./modules/book/book.route')
 
 // apps.use(cors(corsoption));
-apps.use(bodyparser.json());
-apps.use(bodyparser.urlencoded({ extended: false }));
+apps.use(bodyparser.json())
+apps.use(bodyparser.urlencoded({ extended: false }))
 
 // set routing
 // apps.use('/api/books', book)
