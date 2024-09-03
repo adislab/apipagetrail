7b5b8330e6862daa5c1a138f147f2d481d87fe34
Adis <archive.hadisty@gmail.com>
2024-09-03 09:47:43 +0700

    config project

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+// const cors = require('cors');
+const apps = express();
+const PORT = process.env.PORT || 8080;
+// const corsoption = { origin: 'http://localhost:3000' }
+
+// router
+// const book = require('./modules/book/book.route')
+
+// apps.use(cors(corsoption));
+apps.use(bodyparser.json());
+apps.use(bodyparser.urlencoded({ extended: false }));
+
+// set routing
+// apps.use('/api/books', book)
+
+apps.listen(PORT, () => {
+    console.log(`Server Running At : ${PORT}`)
+})
